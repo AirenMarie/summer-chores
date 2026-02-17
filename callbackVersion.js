68bf0a77fd78105d8d4060c6bf51b5250fda5693
AirenMarie <astringer92@gmail.com>
2026-02-16 21:35:42 -0500

    add callbacks

---
+++ callbackVersion.js
@@ -0,0 +1,71 @@
+let isChoreDone = false;
+let allChoresDone = false;
+
+const mowYard = (name, callback) => {
+  setTimeout(() => {
+    isChoreDone = true;
+    console.log(`${name} mowed the yard.`);
+    callback();
+  }, 2000);
+};
+
+const weedEat = (name, callback) => {
+  setTimeout(() => {
+    if (isChoreDone === true) {
+      console.log(`${name} finished using the weed eater.`);
+      callback();
+    } else {
+      console.log(`${name} fell asleep after mowing the yard.`);
+    }
+  }, 1500);
+};
+
+const trimHedges = (name, callback) => {
+  setTimeout(() => {
+    if (isChoreDone === true) {
+      console.log(`${name} finished trimming the hedges.`);
+      callback();
+    } else {
+      console.log(`${name} fell asleep after weed eating the yard.`);
+    }
+  }, 1000);
+};
+
+const collectWood = (name, callback) => {
+  setTimeout(() => {
+    if (isChoreDone === true) {
+      console.log(`${name} finished collecting wood.`);
+      callback();
+    } else {
+      console.log(`${name} fell asleep after trimming the hedges.`);
+    }
+  }, 2500);
+};
+
+const waterGarden = (name, callback) => {
+  setTimeout(() => {
+    if (isChoreDone === true) {
+      console.log(`${name} finished watering the garden.`);
+      callback();
+    } else {
+      console.log(`${name} fell asleep after collecting wood.`);
+    }
+  }, 500);
+};
+
+const doSummerChores = (name) => {
+  mowYard(name, () => {
+    weedEat(name, () => {
+      trimHedges(name, () => {
+        collectWood(name, () => {
+          waterGarden(name, () => {
+            allChoresDone = true;
+            console.log(`${name} finished all of their chores!`);
+          });
+        });
+      });
+    });
+  });
+};
+
+doSummerChores('Felix');
